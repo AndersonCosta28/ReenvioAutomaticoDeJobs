502ae296e94c94d524b9aa4bc8dd9b3cb6221a17
andersoncosta28ss <anderson.costa@sossego.com.br>
2023-01-17 14:06:41 -0300

    using class

--- handlefile.py
+++ handlefile.py
@@ -1,66 +1,68 @@
 import json
 import uuid
+class HandleJobsCharge:
+    id_charge = str(uuid.uuid4())
+    
+    def generateStatus(self):
+        from random import randint
+        value = randint(1, 3)
+        if (value == 1):
+            return "running"
+        elif (value == 2):
+            return "done"
+        else:
+            return "failed"
 
-def generateStatus():
-    from random import randint
-    value = randint(1, 3)
-    if (value == 1):
-        return "running"
-    elif (value == 2):
-        return "done"
-    else:
-        return "failed"
+    def generateOneJob(self, id_parent = ""): 
+        return {
+                "id_job": str(uuid.uuid4()),
+                "id_charge": self.id_charge,
+                "status": self.generateStatus(),
+                "was_sent" : False,
+                "id_parent": id_parent
+            }
 
-def generateJob(idCharge, id_parent = ""): 
-    return {
-            "id_job": str(uuid.uuid4()),
-            "id_charge": str(idCharge),
-            "status": generateStatus(),
-            "was_sent" : False,
-            "id_parent": id_parent
-        }
+    def generateChargeJobs(self):
+        listObjects = []
+        for i in range(0, 12):
+            listObjects.append(self.generateOneJob())
+        return listObjects
 
-def generateChargeJobs(idCharge):
-    listObjects = []
-    for i in range(0, 12):
-        listObjects.append(generateJob(idCharge))
-    return listObjects
+    def updateFile(self, jobs):
+        jsonString = json.dumps(jobs)
+        jsonFile = open(self.id_charge + ".json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
 
-def updateFile(jobs):
-    jsonString = json.dumps(jobs)
-    jsonFile = open("data.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
+    def createFile(self):
+        jsonString = json.dumps(self.generateChargeJobs())
+        jsonFile = open(self.id_charge + ".json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
 
-def createFile(id_charge):
-    jsonString = json.dumps(generateChargeJobs(id_charge))
-    jsonFile = open("data.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
+    def getFailedJobs(self, jobs): 
+        failedJobs = []
+        for i in range(0, len(jobs)):
+            job = jobs[i]
+            isPendent = job['status'] == "failed"
+            wasAlreadySent = job['was_sent'] == False
+            if (isPendent and wasAlreadySent):
+                failedJobs.append({ "job": job, "index": i })
+        return failedJobs
 
-def getFailedJobs(jobs): 
-    failedJobs = []
-    for i in range(0, len(jobs)):
-        job = jobs[i]
-        isPendent = job['status'] == "failed"
-        wasAlreadySent = job['was_sent'] == False
-        if (isPendent and wasAlreadySent):
-            failedJobs.append({ "job": job, "index": i })
-    return failedJobs
+    def resendJobsFailed(self, failedJobs, allJobs):
+        jobsResent = []
+        for i in range(0, len(failedJobs)):
+            indexJob = failedJobs[i]["index"]
+            job = allJobs[indexJob]
+            job['was_sent'] = True
+            jobsResent.append(self.generateOneJob(job['id_job']))
+        return jobsResent
 
-def resendJobsFailed(failedJobs, allJobs):
-    jobsResent = []
-    for i in range(0, len(failedJobs)):
-        indexJob = failedJobs[i]["index"]
-        job = allJobs[indexJob]
-        job['was_sent'] = True
-        jobsResent.append(generateJob(job['id_charge'], job['id_job']))
-    return jobsResent
-
-def updateRunningJobs(jobs):
-    for i in range(0, len(jobs)):
-        
-        isRunning = jobs[i]['status'] == "running"
-        if (isRunning):
-           jobs[i]['status'] = generateStatus()
-    return jobs
+    def updateRunningJobs(self, jobs):
+        for i in range(0, len(jobs)):
+            
+            isRunning = jobs[i]['status'] == "running"
+            if (isRunning):
+                jobs[i]['status'] = self.generateStatus()
+        return jobs
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import handlefile
+from handlefile import HandleJobsCharge
 import json
 import uuid
 import time
@@ -6,23 +6,24 @@ from os import path
 
 
 def main():
+    handleJobsCharge = HandleJobsCharge()
     while (True):
         time.sleep(5) # Simular o x tempo para cada execução
-        jsonFileExists = path.isfile('./data.json')
+        jsonFileExists = path.isfile(handleJobsCharge.id_charge + '.json')
         if (not jsonFileExists): # Aqui simulados a primeira carga, que o corretor acaba de cadastrar a credential
-            handlefile.createFile(uuid.uuid4())
+            handleJobsCharge.createFile()
             continue
 
         if (jsonFileExists):     # Numa segunda execução deste script seria uma simulação de a cada x tempo, iremos verificar se o job foi concluído
-            file = open("data.json", "r")        
+            file = open(handleJobsCharge.id_charge + ".json", "r")        
             content = json.loads(file.read())
-            content = handlefile.updateRunningJobs(content) # Vamos atualizar os jobs que ainda estão rodando
-            failedJobs = handlefile.getFailedJobs(content) # Pegar os Jobs que falharam
-            resentJobs = handlefile.resendJobsFailed(failedJobs, content) # Reenviamos os Jobs falhados
+            content = handleJobsCharge.updateRunningJobs(content) # Vamos atualizar os jobs que ainda estão rodando
+            failedJobs = handleJobsCharge.getFailedJobs(content) # Pegar os Jobs que falharam
+            resentJobs = handleJobsCharge.resendJobsFailed(failedJobs, content) # Reenviamos os Jobs falhados
             newContentToAppend = content + resentJobs # Refazemos a lista para escrever no JSON
 
             file.close()
-            handlefile.updateFile(newContentToAppend)
+            handleJobsCharge.updateFile(newContentToAppend)
             if (len(failedJobs) == 0):
                 break
 
